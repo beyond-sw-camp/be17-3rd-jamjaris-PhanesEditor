d91ecc6c60ebf362c7c7d4e2509367b2fab3615d
lee <why48382@gmail.com>
2025-09-23 11:37:08 +0900

    배포 녹화

--- src/main/java/org/example/coding_convention/test/TestController.java
+++ src/main/java/org/example/coding_convention/test/TestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public BaseResponse<String> test(){
-        return BaseResponse.success("test2");
+        return BaseResponse.success("test3");
     }
 
     @GetMapping("/health")
